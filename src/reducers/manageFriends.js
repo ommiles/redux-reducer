b1d3ae3484735dd8cc864415620ddc9ca150208f
OM Miles <miles@ommiles.com>
2021-06-20 12:49:24 -0700

    commit tree 6f3c6a7c27540c7d5e081760eab5b43335d45b1e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend] };
+    case 'REMOVE_FRIEND':
+      // The findIndex() method returns the index of the first element in the array that satisfies the provided testing function.
+      // Otherwise, it returns -1, indicating that no element passed the test.
+      const removalIndex = state.friends.findIndex(
+        (friend) => friend.id === action.id
+      );
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, removalIndex),
+          ...state.friends.slice(removalIndex + 1),
+        ],
+      };
+    default:
+      return state;
+  }
 }
